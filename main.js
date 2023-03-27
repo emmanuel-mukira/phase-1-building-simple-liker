f8b2a0d289ea4fd74451a0336fa667658a462f2c
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-03-27 16:26:43 +0300

    Done

--- main.js
+++ main.js
@@ -3,8 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const error=document.getElementById('modal');
+error.setAttribute('class','hidden');
+function removeError(){
+  error.removeAttribute('hidden');
+}
 
+const heartIcon = document.querySelectorAll('.like-glyph');
+const errorModal = document.getElementById('error-modal');
+const errorMessage = errorModal.querySelector('.error-message');
+heartIcons.forEach(heartIcon => {
+  heartIcon.addEventListener('click', () => {
+    heartIcon.FULL_HEART;
+  });
 
+  heartIcon.addEventListener('dblclick', () => {
+   heartIcon.EMPTY_HEART;
+  });
+});
+
+// When a user clicks on an empty heart
+heartIcon.addEventListener('click', () => {
+  mimicServerCall()
+    .then(() => {
+      // When the "server" returns a success status
+      // Change the heart to a full heart
+      heartIcon.classList.add('activated-heart');
+    })
+    .catch((error) => {errorModal.classList.remove('hidden');
+    // Display the server error message in the modal
+    errorMessage.textContent = error;
+    // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+    setTimeout(() => {
+      errorModal.classList.add('hidden');
+    }, 3000);
+  });
+});
+// When a user clicks on a full heart
+heartIcon.addEventListener('dblclick', () => {
+// Change the heart back to an empty heart
+heartIcon.classList.remove('activated-heart');
+});
+ 
 
 
 //------------------------------------------------------------------------------
